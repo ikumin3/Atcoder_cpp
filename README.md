fa4f78e5f68e3ec0a411b23c0582e9d60236f7f2
ikumin3 <primeclover193@gmail.com>
2023-04-30 01:54:23 +0900

    m

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ https://qiita.com/togatoga/items/3e8fd0042dc8be702201 <br>
 アーカイブ後、手動でpushすることで、githubに反映できます。　<br>
 
 ## コマンド
+wsl環境だとパスを通していないので、とりあえずWindows上でやって良いかも。<br>
 
 procon-gardener archive　<br>
 git add .　<br>
 git commit -m "-"　<br>
 git push origin main　<br>
+
+また、コンパイルは下記の通り<br>
+
+g++ code.cpp <br>
--- code.cpp
+++ code.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-int N,A[101];
 
 int main() {
-    cin >> N;
-    for(int i=0;i<N;i++) cin >> A[i];
-    sort(A,A+N,greater<int>());
+  int a;
+  cin >> a;
 
-    int v[2] = {0,0};
-    for(int i=0;i<N;i++) v[i%2] += A[i];
+  int b,c;
+  cin >> b >> c;
 
-    int ans = v[0] - v[1];
-    cout << ans << endl;
-    return 0;
+  string s;
+  cin >> s;
+
+  cout << (a+b+c) << " " << s << endl;
+  return 0;
 }
