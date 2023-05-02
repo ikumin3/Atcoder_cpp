b1b57670b8386f7161dd4f179397c981f35837ae
ikumin3 <primeclover193@gmail.com>
2023-05-02 21:10:49 +0900

    -

---
+++ archive/atcoder.jp/abc042/abc042_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int A,B,C;
+ 
+  cin >> A >> B >> C;
+ 
+  if(A > B) swap(A,B);
+  if(C > B) swap(C,B);
+ 
+  if(A == 5 && B == 7 && C == 5) cout << "YES";
+  else cout << "NO";
+ 
+  return 0;
+}
