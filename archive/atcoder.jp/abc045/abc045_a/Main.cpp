8d45bfe36eff7d078dbc0b8dbdaad20d4d404d46
ikumin3 <primeclover193@gmail.com>
2023-05-07 21:30:09 +0900

    -

---
+++ archive/atcoder.jp/abc045/abc045_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a,b,h;
+  int area;
+
+  cin >> a;
+  cin >> b;
+  cin >> h;
+
+  area = (a + b) * h / 2;
+
+  cout << area << endl;
+}
